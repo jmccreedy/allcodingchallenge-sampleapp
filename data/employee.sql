eb99a535febcb7e056bf366d876716c11982993d
Jordan Irvine <irvine-j14@ulster.ac.uk>
2021-03-30 12:14:31 +0100

    test_3

--- data/employee.sql
+++ data/employee.sql
@@ -63,3 +63,5 @@ INSERT INTO `employeedata` (`id`, `employee_id`, `employee_name`, `age`, `gender
 -- /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 -- /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 -- /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- /*!test */;
